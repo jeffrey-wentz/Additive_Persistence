d589502c2d2dd8f8cb607a8f27bfb838c39e5f84
jeffrey-wentz <jeff.a.wentz@gmail.com>
2019-03-27 13:34:38 -0700

    created recursive solution

--- main2.py
+++ main2.py
@@ -1,26 +1,24 @@
-number = 0
+def additive_persistence(num, counter, new_num):
+    for each_digit in str(num):
+        new_num += int(each_digit)
+    counter += 1
+    if new_num > 9:
+        print("New Function Call")
+        additive_persistence(new_num, counter, 0)
+    else:
+        print("Additive persistence:  " + str(counter))
+        return 0
+
+
+# new_num = 0
 success = 0
-additive_persistence = 0
+counter = 0
+user_num = 0
 while success == 0:
     try:
-        number = int(input("please input your number: "))
+        user_num = int(input("please input your number: "))
         success = 1
     except ValueError:
         print("Must be a number yo")
 
-while number > 9:
-    if number > 99:
-        # I thought this was pretty funny to try, oh well!
-        number = int(str(number[0])) + int(str(number[1]))
-    if number > 999:
-        pass
-    if number > 9999:
-        pass
-    if number > 99999:
-        pass
-    if number > 999999:
-        pass
-    if number > 9999999:
-        pass
-
-print("Additive persistence:  " + str(additive_persistence))
+additive_persistence(user_num, counter, 0)
